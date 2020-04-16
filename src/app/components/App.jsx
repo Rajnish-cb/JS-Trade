7695b6d2502d773d5451a42757a970b2eb20721a
Rajnish <rajnish@cueblocks.com>
2020-04-16 15:35:43 +0530

    initial commit

---
+++ src/app/components/App.jsx
@@ -0,0 +1,15 @@
+import React from 'react';
+
+import { Glyphicon } from 'react-bootstrap';
+
+class App extends React.Component {
+  render() {
+    return (
+      <div>
+        <Glyphicon glyph="star" />
+
+        Hello React!
+      </div>
+    );
+  }
+}
